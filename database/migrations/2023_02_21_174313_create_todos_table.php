643eb3903c5e2b79ec835b46737569543c16d929
Taha Çiçek <ttahacicek@gmail.com>
2023-02-21 21:04:10 +0300

    started user project page. with todo and invite code.

---
+++ database/migrations/2023_02_21_174313_create_todos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->dateTime('warning_date')->nullable();
+            $table->enum('priority', ['normal', 'starred'])->default('normal');
+            $table->enum('status', ['pending', 'completed', 'canceled'])->default('pending');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
---
+++ database/migrations/2023_02_21_175012_create_projects_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->enum('category', ['personal', 'work', 'other']);
+            $table->text('description');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('color')->nullable();
+            $table->date('deadline')->nullable();
+            $table->enum('status', ['pending', 'completed', 'active']);
+            $table->boolean('is_public')->default(false);
+            $table->boolean('priority')->default(false);
+            $table->string('invite_code')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
